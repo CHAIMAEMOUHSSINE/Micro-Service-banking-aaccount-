14fca157660cf3d5cb3ecb4af5c7fcec3f80e294
Chaimae Mouhssine <chaimaemouhssine60@gmail.com>
2026-09-24 21:09:11 +0000

    graphQl

---
+++ src/main/java/net/chaimae/bankaccountservice/web/BankAccountGraphQlController.java
@@ -0,0 +1,21 @@
+package net.chaimae.bankaccountservice.web;
+
+import net.chaimae.bankaccountservice.entities.BankAccount;
+import net.chaimae.bankaccountservice.repositories.BankAccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class BankAccountGraphQlController {
+    @Autowired
+    private BankAccountRepository bankAccountRepository ;
+    @QueryMapping
+    public List<BankAccount> accountList(){
+        return bankAccountRepository.findAll();
+    }
+
+
+}
