a7d15ceb112ae94adef666f062cdf152798eca79
Chaimae Mouhssine <chaimaemouhssine60@gmail.com>
2026-09-26 11:10:17 +0000

    coorecting issues and setting the mutation mapping

--- src/main/java/net/chaimae/bankaccountservice/web/BankAccountGraphQlController.java
+++ src/main/java/net/chaimae/bankaccountservice/web/BankAccountGraphQlController.java
@@ -1,21 +1,48 @@
 package net.chaimae.bankaccountservice.web;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import net.chaimae.bankaccountservice.dto.BankAccountRequestDTO;
+import net.chaimae.bankaccountservice.dto.BankAccountResponseDTO;
 import net.chaimae.bankaccountservice.entities.BankAccount;
+import net.chaimae.bankaccountservice.enums.AccountType;
 import net.chaimae.bankaccountservice.repositories.BankAccountRepository;
+import net.chaimae.bankaccountservice.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
+import java.util.Date;
 import java.util.List;
 
 @Controller
 public class BankAccountGraphQlController {
     @Autowired
     private BankAccountRepository bankAccountRepository ;
+    @Autowired
+    public AccountService accountService;
     @QueryMapping
     public List<BankAccount> accountsList() {
         return bankAccountRepository.findAll();
     }
+    @QueryMapping
+    public BankAccount bankAccounById(String id){
+        return bankAccountRepository.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException(String.format("Account %s not found", id))
+                );
+    }
+    @MutationMapping
+    public BankAccountResponseDTO addAccount(@Argument BankAccountRequestDTO bankAccount) {
 
+        return accountService.addAccount(bankAccount);
+    }
+
+
+/*record BankAccountDTO (Double balance, String currency, String type) {}*/
 
 }
